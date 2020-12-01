4b154a498ee305675c0fd688d39313b73ff8311f
John Owen <john.owen@aginic.com>
2020-12-01 17:09:03 +1000

    refactor test

--- functions/rss-to-s3/feedToCsv.test.ts
+++ functions/rss-to-s3/feedToCsv.test.ts
@@ -8,30 +8,30 @@ describe("feedToCsv", () => {
         { title: "Queen of England releases rap album" },
       ],
     };
-    expect(feedToCsv(feed)).toEqual(
-      `"title"
-"Florida man strangles beetle"
-"Queen of England releases rap album"`
-    );
+    expect(feedToCsv(feed)).toMatchInlineSnapshot(`
+      "\\"title\\"
+      \\"Florida man strangles beetle\\"
+      \\"Queen of England releases rap album\\""
+    `);
   });
 
   it("should handle a feed which is missing the items field", () => {
     const feed = {};
-    expect(feedToCsv(feed)).toEqual(`"title"`);
+    expect(feedToCsv(feed)).toMatchInlineSnapshot(`"\\"title\\""`);
   });
 
   it("should handle a feed with 0 items in the items field", () => {
     const feed = { items: [] };
-    expect(feedToCsv(feed)).toEqual(`"title"`);
+    expect(feedToCsv(feed)).toMatchInlineSnapshot(`"\\"title\\""`);
   });
 
   it("should handle items with missing titles", () => {
     const feed = {
       items: [{}, { title: "Queen of England releases rap album" }],
     };
-    expect(feedToCsv(feed)).toEqual(
-      `"title"
-"Queen of England releases rap album"`
-    );
+    expect(feedToCsv(feed)).toMatchInlineSnapshot(`
+      "\\"title\\"
+      \\"Queen of England releases rap album\\""
+    `);
   });
 });
